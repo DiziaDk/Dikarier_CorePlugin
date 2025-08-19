c49cc30c605bd8e6eb7b35dcc19f16f05aae0950
DiziaDk <artemforg26@gmail.com>
2025-08-19 19:26:48 +0300

    Update Dikarier_Core.js
    fixed local switch timer

--- Dikarier_Core.js
+++ Dikarier_Core.js
@@ -1182,7 +1182,7 @@ const param = PluginManager.parameters(DCore.pluginName);
         const currentFrame = Graphics.frameCount;
         const activeTimers = $gameSystem.activeTimers;
 
-        for (let i = activeTimers.length - 1; i >= 0; i--) {
+        for (let i = $gameSystem.activeTimers.length - 1; i >= 0; i--) {
             const timer = activeTimers[i];
 
             if (currentFrame >= timer.endTime) {
